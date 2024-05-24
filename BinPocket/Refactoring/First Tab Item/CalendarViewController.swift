d2619cf1309fbd9ee96b893802c5472904786ebb
avan31017 <avan31017@gmail.com>
2024-05-24 23:52:53 +0900

    [Refacor-Codebase] - CalendarViewController UI 선언 위치 수정

--- BinPocket/Refactoring/First Tab Item/CalendarViewController.swift
+++ BinPocket/Refactoring/First Tab Item/CalendarViewController.swift
@@ -12,103 +12,146 @@ class CalendarViewController: UIViewController {
     // 달력 컨테이너 시작
     private var calendarCollectionView: UICollectionView!
     
-    private var prevMonthButton: UIButton = {
-        let button = UIButton()
-        var config = UIButton.Configuration.plain()
-        config.image = UIImage(systemName: "chevron.left")
-        button.configuration = config
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
+    private var prevMonthButton: UIButton = UIButton()
     
-    private var currentYearMonth: UILabel = {
-        let label = UILabel()
-        label.text = "yyyy년 mm월"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
+    private var currentYearMonth: UILabel = UILabel()
     
-    private var nextMonthButton: UIButton = {
-        let button = UIButton()
-        var config = UIButton.Configuration.plain()
-        config.image = UIImage(systemName: "chevron.right")
-        button.configuration = config
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
+    private var nextMonthButton: UIButton = UIButton()
     
-    private var todayButton: UIButton = {
-        let button = UIButton()
-        button.setTitleColor(.white, for: .normal)
-        var config = UIButton.Configuration.filled()
-        config.title = "Today"
-        config.background.backgroundColor = .black
-        button.configuration = config
-        button.addAction(UIAction { _ in
-            print("Clicked Today Button.")
-        }, for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-    
-    private lazy var calendarNavigationBarStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.alignment = .center
-        stackView.distribution = .fill
-        stackView.spacing = 10
-        
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.addArrangedSubview(prevMonthButton)
-        stackView.addArrangedSubview(currentYearMonth)
-        stackView.addArrangedSubview(nextMonthButton)
+    private var todayButton: UIButton = UIButton()
 
-        return stackView
-    }()
+    private lazy var calendarNavigationBarStackView: UIStackView = UIStackView()
+
+    private lazy var calendarView: UIView = UIView()
+    // 달력 컨테이너 끝
     
-    private lazy var calendarView: UIView = {
-        let calendarView = UIView()
-        calendarView.backgroundColor = .yellow
+
+
+    // 수입 View
+    private lazy var inComeAmountLabel: UILabel = UILabel()
+    
+    private lazy var inComeAmountView: UIView = UIView()
+    
+    // 지출 View
+    private lazy var expenseAmountLabel: UILabel = UILabel()
+
+    private lazy var expenseAmountView: UIView = UIView()
+
+    // Total View
+    private lazy var totalAmountView: UIView = UIView()
+    
+    // 요일 스택뷰
+    private lazy var weekOfDayStackView: UIStackView = UIStackView()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
         
-        calendarView.translatesAutoresizingMaskIntoConstraints = false
+        setupCalendarNavBarStackView()
+        setupTodayButton()
+        setupWeekOfDayStackView()
+        setupCalendarView()
+        setupInComeAmountView()
+        setupExpenseAmountView()
+        setupTotalAmountView()
+
+    }
+    
+    // UI 레이아웃 메소드
+    func setupCalendarNavBarStackView() {
+        prevMonthButton.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        currentYearMonth.text = "yyyy년 mm월"
+        nextMonthButton.setImage(UIImage(systemName: "chevron.right"), for: .normal)
+        
+        calendarNavigationBarStackView.axis = .horizontal
+        calendarNavigationBarStackView.alignment = .center
+        calendarNavigationBarStackView.distribution = .fill
+        calendarNavigationBarStackView.spacing = 10
+        
+        calendarNavigationBarStackView.translatesAutoresizingMaskIntoConstraints = false
+        calendarNavigationBarStackView.addArrangedSubview(prevMonthButton)
+        calendarNavigationBarStackView.addArrangedSubview(currentYearMonth)
+        calendarNavigationBarStackView.addArrangedSubview(nextMonthButton)
         
         calendarView.addSubview(calendarNavigationBarStackView)
-        calendarView.addSubview(todayButton)
-        calendarView.addSubview(weekOfDayStackView)
-//        calendarView.addSubview(calendarCollectionView)
-        
         
         NSLayoutConstraint.activate([
             calendarNavigationBarStackView.topAnchor.constraint(equalTo: calendarView.topAnchor),
             calendarNavigationBarStackView.centerXAnchor.constraint(equalTo: calendarView.centerXAnchor),
-            
+        ])
+    }
+    
+    
+    func setupTodayButton() {
+        todayButton.setTitleColor(.white, for: .normal)
+        todayButton.setTitle("Today", for: .normal)
+        todayButton.backgroundColor = .black
+        todayButton.addAction(UIAction { _ in
+            print("Clicked Today Button.")
+        }, for: .touchUpInside)
+        
+        todayButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        calendarView.addSubview(todayButton)
+        
+        NSLayoutConstraint.activate([
             todayButton.topAnchor.constraint(equalTo: calendarNavigationBarStackView.topAnchor),
             todayButton.trailingAnchor.constraint(equalTo: calendarView.trailingAnchor, constant: -5),
+        ])
+    }
+    
+    func setupWeekOfDayStackView() {
+        weekOfDayStackView.axis = .horizontal
+        weekOfDayStackView.distribution = .fillEqually
+        weekOfDayStackView.alignment = .center
+        
+        let weekOfDay: [String] = ["일", "월", "화", "수", "목", "금", "토"]
+        
+        for day in weekOfDay {
+            let weekLabel = UILabel()
+            weekLabel.text = day
+            weekLabel.textAlignment = .center
             
+            if day == "일" {
+                weekLabel.textColor = .red
+            }else if day == "토" {
+                weekLabel.textColor = .blue
+            }
+            
+            weekOfDayStackView.addArrangedSubview(weekLabel)
+        }
+        
+        weekOfDayStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        calendarView.addSubview(weekOfDayStackView)
+        
+        NSLayoutConstraint.activate([
             weekOfDayStackView.topAnchor.constraint(equalTo: todayButton.bottomAnchor, constant: 10),
             weekOfDayStackView.leadingAnchor.constraint(equalTo: calendarView.leadingAnchor),
             weekOfDayStackView.trailingAnchor.constraint(equalTo: calendarView.trailingAnchor)
-            
-//            calendarCollectionView.topAnchor.constraint(equalTo: calendarNavigationBarStackView.bottomAnchor, constant: 5),
-//            calendarCollectionView.leadingAnchor.constraint(equalTo: calendarView.leadingAnchor),
-//            calendarCollectionView.trailingAnchor.constraint(equalTo: calendarView.trailingAnchor),
-//            calendarCollectionView.bottomAnchor.constraint(equalTo: calendarView.bottomAnchor)
         ])
-        return calendarView
-    }()
-    // 달력 컨테이너 끝
-
-    // 수입 View
-    private lazy var inComeAmountLabel: UILabel = {
-        let label = UILabel()
-        label.text = "0"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
+    }
     
-    private lazy var inComeAmountView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .green
+    func setupCalendarView() {
+        calendarView.backgroundColor = .yellow
+        
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(calendarView)
+        
+        NSLayoutConstraint.activate([
+            calendarView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            calendarView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5),
+            calendarView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5),
+            calendarView.heightAnchor.constraint(greaterThanOrEqualToConstant: 300)
+        ])
+    }
+    
+    func setupInComeAmountView() {
+        inComeAmountLabel.text = "0"
+        
+        inComeAmountView.backgroundColor = .green
         
         let inComeTitle = UILabel()
         inComeTitle.text = "수입"
@@ -116,7 +159,7 @@ class CalendarViewController: UIViewController {
         
         let wonLabel = UILabel()
         wonLabel.text = "원"
-    
+        
         let hStackView = UIStackView()
         hStackView.axis = .horizontal
         hStackView.distribution = .fill
@@ -125,31 +168,35 @@ class CalendarViewController: UIViewController {
         hStackView.addArrangedSubview(wonLabel)
         hStackView.translatesAutoresizingMaskIntoConstraints = false
         
-        view.addSubview(inComeTitle)
-        view.addSubview(hStackView)
+        inComeAmountView.addSubview(inComeTitle)
+        inComeAmountView.addSubview(hStackView)
         
+        // inComeAmountView의 내부 레이아웃
         NSLayoutConstraint.activate([
-            inComeTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            inComeTitle.topAnchor.constraint(equalTo: view.topAnchor, constant: 5),
+            inComeTitle.centerXAnchor.constraint(equalTo: inComeAmountView.centerXAnchor),
+            inComeTitle.topAnchor.constraint(equalTo: inComeAmountView.topAnchor, constant: 5),
             
-            hStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            hStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            hStackView.centerXAnchor.constraint(equalTo: inComeAmountView.centerXAnchor),
+            hStackView.centerYAnchor.constraint(equalTo: inComeAmountView.centerYAnchor),
         ])
         
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    // 지출 View
-    private lazy var expenseAmountLabel: UILabel = {
-        let label = UILabel()
-        label.text = "0"
-        return label
-    }()
-    
-    private lazy var expenseAmountView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .green
+        inComeAmountView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(inComeAmountView)
+        
+        // inComeAmountView의 자체 레이아웃
+        NSLayoutConstraint.activate([
+            inComeAmountView.topAnchor.constraint(equalTo: calendarView.bottomAnchor, constant: 10),
+            inComeAmountView.leadingAnchor.constraint(equalTo: calendarView.leadingAnchor),
+            inComeAmountView.trailingAnchor.constraint(equalTo: calendarView.centerXAnchor, constant: -5),
+            inComeAmountView.heightAnchor.constraint(greaterThanOrEqualToConstant: 80),
+        ])
+    }
+
+    func setupExpenseAmountView() {
+        expenseAmountLabel.text = "0"
+        
+        expenseAmountView.backgroundColor = .green
         
         let expenseTitle = UILabel()
         expenseTitle.text = "지출"
@@ -157,7 +204,7 @@ class CalendarViewController: UIViewController {
         
         let wonLabel = UILabel()
         wonLabel.text = "원"
-    
+        
         let hStackView = UIStackView()
         hStackView.axis = .horizontal
         hStackView.distribution = .fill
@@ -166,25 +213,33 @@ class CalendarViewController: UIViewController {
         hStackView.addArrangedSubview(wonLabel)
         hStackView.translatesAutoresizingMaskIntoConstraints = false
         
-        view.addSubview(expenseTitle)
-        view.addSubview(hStackView)
+        expenseAmountView.addSubview(expenseTitle)
+        expenseAmountView.addSubview(hStackView)
         
+        // inComeAmountView의 내부 레이아웃
         NSLayoutConstraint.activate([
-            expenseTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            expenseTitle.topAnchor.constraint(equalTo: view.topAnchor, constant: 5),
+            expenseTitle.centerXAnchor.constraint(equalTo: expenseAmountView.centerXAnchor),
+            expenseTitle.topAnchor.constraint(equalTo: expenseAmountView.topAnchor, constant: 5),
             
-            hStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            hStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            hStackView.centerXAnchor.constraint(equalTo: expenseAmountView.centerXAnchor),
+            hStackView.centerYAnchor.constraint(equalTo: expenseAmountView.centerYAnchor),
         ])
         
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    // Total View
-    private lazy var totalAmountView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .lightGray
+        expenseAmountView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(expenseAmountView)
+        
+        // inComeAmountView의 자체 레이아웃
+        NSLayoutConstraint.activate([
+            expenseAmountView.topAnchor.constraint(equalTo: inComeAmountView.topAnchor),
+            expenseAmountView.leadingAnchor.constraint(equalTo: calendarView.centerXAnchor, constant: 5),
+            expenseAmountView.trailingAnchor.constraint(equalTo: calendarView.trailingAnchor),
+            expenseAmountView.heightAnchor.constraint(greaterThanOrEqualToConstant: 80),
+        ])
+    }
+
+    func setupTotalAmountView() {
+        totalAmountView.backgroundColor = .lightGray
         
         let totalTitle = UILabel()
         totalTitle.text = "총합"
@@ -205,84 +260,28 @@ class CalendarViewController: UIViewController {
         hStackView.addArrangedSubview(wonLabel)
         hStackView.translatesAutoresizingMaskIntoConstraints = false
         
-        view.addSubview(totalTitle)
-        view.addSubview(hStackView)
+        totalAmountView.addSubview(totalTitle)
+        totalAmountView.addSubview(hStackView)
         
         NSLayoutConstraint.activate([
-            totalTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            totalTitle.topAnchor.constraint(equalTo: view.topAnchor, constant: 5),
+            totalTitle.centerXAnchor.constraint(equalTo: totalAmountView.centerXAnchor),
+            totalTitle.topAnchor.constraint(equalTo: totalAmountView.topAnchor, constant: 5),
             
-            hStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            hStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            hStackView.centerXAnchor.constraint(equalTo: totalAmountView.centerXAnchor),
+            hStackView.centerYAnchor.constraint(equalTo: totalAmountView.centerYAnchor),
         ])
         
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    // 요일 스택뷰
-    private lazy var weekOfDayStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.distribution = .fillEqually
-        stackView.alignment = .center
+        totalAmountView.translatesAutoresizingMaskIntoConstraints = false
         
-        let weekOfDay: [String] = ["일", "월", "화", "수", "목", "금", "토"]
-        
-        for day in weekOfDay {
-            let weekLabel = UILabel()
-            weekLabel.text = day
-            weekLabel.textAlignment = .center
-            
-            if day == "일" {
-                weekLabel.textColor = .red
-            }else if day == "토" {
-                weekLabel.textColor = .blue
-            }
-            
-            stackView.addArrangedSubview(weekLabel)
-        }
-        
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
-    }()
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-
-        view.addSubview(calendarView)
-        view.addSubview(inComeAmountView)
-        view.addSubview(expenseAmountView)
         view.addSubview(totalAmountView)
         
         NSLayoutConstraint.activate([
-            calendarView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            calendarView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 5),
-            calendarView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5),
-            calendarView.heightAnchor.constraint(greaterThanOrEqualToConstant: 300),
-            
-            inComeAmountView.topAnchor.constraint(equalTo: calendarView.bottomAnchor, constant: 10),
-            inComeAmountView.leadingAnchor.constraint(equalTo: calendarView.leadingAnchor),
-            inComeAmountView.trailingAnchor.constraint(equalTo: calendarView.centerXAnchor, constant: -5),
-            inComeAmountView.heightAnchor.constraint(greaterThanOrEqualToConstant: 80),
-            
-            expenseAmountView.topAnchor.constraint(equalTo: inComeAmountView.topAnchor),
-            expenseAmountView.leadingAnchor.constraint(equalTo: calendarView.centerXAnchor, constant: 5),
-            expenseAmountView.trailingAnchor.constraint(equalTo: calendarView.trailingAnchor),
-            expenseAmountView.heightAnchor.constraint(greaterThanOrEqualToConstant: 80),
-            
             totalAmountView.topAnchor.constraint(equalTo: expenseAmountView.bottomAnchor, constant: 10),
             totalAmountView.leadingAnchor.constraint(equalTo: inComeAmountView.leadingAnchor),
             totalAmountView.trailingAnchor.constraint(equalTo: expenseAmountView.trailingAnchor),
             totalAmountView.heightAnchor.constraint(greaterThanOrEqualToConstant: 80),
             totalAmountView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10)
-
         ])
-        
     }
-    
-
 }
 
--- Pods-BinPocket/Pods-BinPocket-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BinPocket : NSObject
-@end
-@implementation PodsDummy_Pods_BinPocket
-@end
