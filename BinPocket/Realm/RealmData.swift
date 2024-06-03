e4353c5a1debcce32031dfec9eb2451786d52885
avan31017 <avan31017@gmail.com>
2024-06-03 15:25:07 +0900

    edit code

--- BinPocket/Realm/RealmData.swift
+++ BinPocket/Realm/RealmData.swift
@@ -11,7 +11,7 @@ import RealmSwift
 //Realm에 저장될 데이터 모델!
 class MyData: Object {
     @objc dynamic var date = "" //'yyyy년 M월 d일' 형식으로 들어갈 예정.
-    @objc dynamic var plusOrMinus = false //지출/수입 구분.  false -> 수입, true -> 지출.
+    @objc dynamic var plusOrMinus = false //지출/수입 구분. false -> 수입, true -> 지출.
     @objc dynamic var moneyTitle = ""
     @objc dynamic var money = ""
     @objc dynamic var id = ""
--- BinPocket/Refactoring/First Tab Item/CalendarViewController.swift
+++ BinPocket/Refactoring/First Tab Item/CalendarViewController.swift
@@ -55,7 +55,7 @@ class CalendarViewController: UIViewController {
         // 현재 년,월 타이틀 update
         CalendarDateManager.manager.updateTitleLabel(currentYearMonth)
         
-        print(RealmDataManager.manager.getAllMyData())
+//        print(RealmDataManager.manager.getAllMyData())
     }
     
     // UI 레이아웃 배치 메소드
